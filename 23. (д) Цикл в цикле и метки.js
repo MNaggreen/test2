4653fca32bac6bc9c4e3061dfe70f71ef41850a8
Nikolay <nickolas1594@mail.ru>
2022-12-14 16:50:36 +0300

    23 tasks

--- 23. (д) Цикл в цикле и метки.js
+++ 23. (д) Цикл в цикле и метки.js
@@ -148,5 +148,180 @@ Third level: 1
 
 
 
-for (let i = 5; i <= 10; i++ ){
-    console.log(i);}
+/* Задачи */
+
+// Место для первой задачи
+function firstTask() {
+    for (let i = 5; i <= 10; i++ ){
+        console.log(i);
+    }
+    // Пишем решение вот тут
+    
+    
+}
+
+// Место для второй задачи
+function secondTask() {
+    for (let i = 20; i >= 10; i-- ){
+        if (i === 13) {
+         break;
+     } else {
+         console.log(i);
+     }
+    // Пишем решение вот тут
+    }
+}
+
+// Место для третьей задачи
+function thirdTask() {
+    for (let i = 2; i <= 10; i++ ){
+        if (i % 2 === 0) {
+         console.log(i);
+     } else {
+         continue; }
+    // Пишем решение вот тут
+    
+    }
+}
+
+// Место для четвертой задачи
+
+// Цикл, который нужно переписать:
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+function fourthTask() {
+    let  num = 2;
+    while (num <= 15){
+        num++;
+        if (num % 2 !== 0) {
+        console.log(num);
+        } else {
+            continue;
+        }
+        
+    }
+    // Пишем решение вот тут
+    
+    
+}
+
+// Место для пятой задачи
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+    for (let i = 5; i <= 10; i++)
+    {
+    arrayOfNumbers[i - 5] = i;
+    // Пишем решение вот тут
+    }
+    
+    // Не трогаем
+    return arrayOfNumbers;
+}
+
+/* test */
+const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+    const len = arr.length;
+    // Пишем решение вот тут
+    for (let i = 0; i < len; i++) {
+        result[i] = arr[i];
+        
+    }
+    console.log(result);
+
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const lens = data.length;
+    // Пишем решение вот тут
+    for (let i = 0; i < lens; i++) {
+        if (typeof(data[i]) === 'number'){
+            data[i] = data[i] * 2;
+        } else {
+                data[i] = data[i] + ' - done';
+            }
+    }
+    console.log(data);
+    const res = typeof('K')
+    console.log(typeof(res));
+
+
+    // Место для первой задачи
+function firstTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+    const len = arr.length;
+    // Пишем решение вот тут
+    for (let i = 0; i < len; i++) {
+        result[i] = arr[i];
+    }
+    // Не трогаем
+    return result;
+}
+
+// Место для второй задачи
+function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const lens = data.length;
+    // Пишем решение вот тут
+    for (let i = 0; i < lens; i++) {
+        if (typeof(data[i]) === 'number'){
+            data[i] = data[i] * 2;
+        } else {
+                data[i] = data[i] + ' - done';
+            }
+    }
+    // Не трогаем
+    return data;
+}
+
+// Место для третьей задачи
+function thirdTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+    const lend = data.length - 1;
+    let count = 0;
+    // Пишем решение вот тут
+    for (let i = lend; i >= 0; i--) {        
+            result[count] = data[i];
+            count++;              
+    }
+    
+    
+    // Не трогаем
+    return result;
+}
+
+
+
+const lines = 5;
+let result = '';
+let count = 5;
+let str = ' ';
+// Проверяется именно переменная result, формируйте строку в ней
+for  (let i = 0; i <= lines + 6; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+    result += str.repeat(count);
+    count--;    
+    for  (let j = 2; j <= i+1; j++) {
+        
+        result += '*';
+               
+    }
+    result += '\n';
+}
+
+}
+console.log(result);
+
