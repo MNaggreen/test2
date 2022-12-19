bf7913535c9e10198c7cd171d1b2af99f725e21a
Nikolay <nickolas1594@mail.ru>
2022-12-19 17:52:41 +0300

    27

--- 27. (д) Про важность return.js
+++ 27. (д) Про важность return.js
@@ -41,3 +41,62 @@ test();
 /* здесь ключевую роль играет return он работает как
 break т.е. после него ничего выводиться даже наша
 надпись Done */
+
+
+
+function doNothing() {};
+console.log(doNothing());
+/* undefined */
+/* все функции всегда что то возвращают */
+
+console.log(11);
+/* 11 
+undefined
+тольуо в браузере такое может быть
+потомучто console.log()
+это тоже функция*/
+
+
+
+/* после return никогда нельзя ставить перенос строки
+т.е. так 
+return
+ 1+ 2 
+ делать нельзя
+ return 1+2 можно*/
+
+
+
+ 
+// Место для первой задачи
+function sayHello(name) {
+    return `Привет, ${name}!`
+
+}
+
+// Место для второй задачи
+function returnNeighboringNumbers(num) {
+    let res = []
+    res[0] = num - 1
+    res[1] = num
+    res[2] = num + 1
+    return res
+
+}
+
+// Место для третьей задачи
+function getMathResult(a, b) {
+    let res = '';
+    let count = a;
+    if (b > 0){
+    for (let i = 1; i <= b; i++) {
+        res += `${count}---`;
+        count += a;
+    }
+    res = res.slice(0, -3);} 
+    /* вырезаем не нужные минуса способом индекса */
+    else {
+        res = a;
+    }
+    return res;    
+}
